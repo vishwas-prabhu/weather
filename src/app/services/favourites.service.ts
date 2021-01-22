8462a12b59c035e0e8331f94e144bb312baaf0bf
vishwas-prabhu <vishwas.prabhu@robosoftin.com>
2021-01-22 14:46:17 +0530

    caching is added to favourites

--- src/app/services/favourites.service.ts
+++ src/app/services/favourites.service.ts
@@ -10,23 +10,42 @@ import { StorageService } from './storage.service';
 export class FavouritesService {
 
   list: Weather[] | any = [];
-
+  timeOut: any;
+  listCache = new Map();
+  baseUrl = 'https://api.openweathermap.org/data/2.5/';
+  apiKey = '&appid=fd1aff577261f0d57958b40b645a4145';
   constructor(
     private http: HttpClient,
     private storageService: StorageService
-  ) { }
+  ) {
+  }
+
+  setTimer(): void {
+    this.timeOut = setTimeout(() => {
+      this.listCache.clear();
+    }, 360000);
+  }
 
   setFavouritesList(type: string): void {
     this.list.splice(0, this.list.length);
     if (type === 'fav' && this.storageService.getFavouritesList()) {
-      this.http.get<any>(`https://api.openweathermap.org/data/2.5/group?id=${this.storageService.getFavouritesList()}&appid=fd1aff577261f0d57958b40b645a4145`)
+      const favList = this.storageService.getFavouritesList();
+      const beersFromCache = this.listCache.get(`${favList}`);
+      if (beersFromCache) {
+        this.list.push(...beersFromCache);
+        return;
+      }
+      this.http.get<any>(`${this.baseUrl}group?id=${favList}${this.apiKey}`)
       .subscribe(
         (response: any) => {
+          this.listCache.set(`${favList}`, response.list);
           this.list.push(...response.list);
+          clearTimeout(this.timeOut);
+          this.setTimer();
         }
       );
     } else if (type === 'rec' && this.storageService.getRecentList()) {
-      this.http.get<any>(`https://api.openweathermap.org/data/2.5/group?id=${this.storageService.getRecentList()}&appid=fd1aff577261f0d57958b40b645a4145`).subscribe(
+      this.http.get<any>(`${this.baseUrl}group?id=${this.storageService.getRecentList()}${this.apiKey}`).subscribe(
         (response: any) => {
           this.list.push(...response.list);
         }
